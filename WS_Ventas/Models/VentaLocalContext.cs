4dbf10703bd417dcb4fa3056a6920958a4f92c02
navudzemog <gomez.duvan@outlook.com>
2021-12-26 12:38:42 -0500

    primera version del proyecto

---
+++ WS_Ventas/Models/VentaLocalContext.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace WS_Ventas.Models
+{
+    public partial class VentaLocalContext : DbContext
+    {
+        public VentaLocalContext()
+        {
+        }
+
+        public VentaLocalContext(DbContextOptions<VentaLocalContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Cliente> Cliente { get; set; }
+        public virtual DbSet<Concepto> Concepto { get; set; }
+        public virtual DbSet<Producto> Producto { get; set; }
+        public virtual DbSet<Venta> Venta { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=localhost,1433;Initial Catalog=VentaLocal;Persist Security Info=False;User ID=sa;Password=R0b0ts@2021;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cliente>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasColumnName("nombre")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<Concepto>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Cantidad).HasColumnName("cantidad");
+
+                entity.Property(e => e.IdProducto).HasColumnName("id_producto");
+
+                entity.Property(e => e.IdVenta).HasColumnName("id_venta");
+
+                entity.Property(e => e.Importe)
+                    .HasColumnName("importe")
+                    .HasColumnType("decimal(16, 2)");
+
+                entity.Property(e => e.PrecioUnitario)
+                    .HasColumnName("precioUnitario")
+                    .HasColumnType("decimal(16, 2)");
+
+                entity.HasOne(d => d.IdProductoNavigation)
+                    .WithMany(p => p.Concepto)
+                    .HasForeignKey(d => d.IdProducto)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Concepto__id_pro__4D94879B");
+
+                entity.HasOne(d => d.IdVentaNavigation)
+                    .WithMany(p => p.Concepto)
+                    .HasForeignKey(d => d.IdVenta)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Concepto__id_ven__4CA06362");
+            });
+
+            modelBuilder.Entity<Producto>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Costo)
+                    .HasColumnName("costo")
+                    .HasColumnType("decimal(16, 0)");
+
+                entity.Property(e => e.Nombre)
+                    .IsRequired()
+                    .HasColumnName("nombre")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PrecioUnitario)
+                    .HasColumnName("precioUnitario")
+                    .HasColumnType("decimal(16, 2)");
+            });
+
+            modelBuilder.Entity<Venta>(entity =>
+            {
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Fecha)
+                    .HasColumnName("fecha")
+                    .HasColumnType("datetime");
+
+                entity.Property(e => e.IdCliente).HasColumnName("id_cliente");
+
+                entity.Property(e => e.Total)
+                    .HasColumnName("total")
+                    .HasColumnType("decimal(16, 2)");
+
+                entity.HasOne(d => d.IdClienteNavigation)
+                    .WithMany(p => p.Venta)
+                    .HasForeignKey(d => d.IdCliente)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Venta__id_client__49C3F6B7");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
